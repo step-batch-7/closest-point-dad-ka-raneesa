4bd66d60d90243a3b159e29a765e67167487bd7e
Deepika <bhuvanadeepika017@gmail.com>
2020-04-13 12:11:31 +0530

    added modified main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void)
   Point organism_locations[LENGTH] = ORGANISM_LOCATIONS;
   Point closest_food_location = {0, 0};
 
-  for (int i = 0; i < LENGTH; i++)
+  FOR(o, LENGTH)
   {
     get_closest_food(food_points, LENGTH, organism_locations[i], &closest_food_location);
     printf("Location of organism: [%d %d],  Closest food target: [%d %d]\n", organism_locations[i].x, organism_locations[i].y, closest_food_location.x, closest_food_location.y);
