3886172f5a1a76debfa9896f1d9442766845be9f
Deepika <bhuvanadeepika017@gmail.com>
2020-04-13 11:41:53 +0530

    modified main.c

--- main.c
+++ main.c
@@ -6,8 +6,11 @@ int main(void)
   struct Point organism_locations[5] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
   struct Point closest_food_location = {0, 0};
 
-  get_closest_food(food_points, 5, organism_locations[0], &closest_food_location);
-  printf("Location of organism: [%d %d],  Closest food target: [%d %d]\n", organism_locations[0].x, organism_locations[0].y, closest_food_location.x, closest_food_location.y);
+  for (int i = 0; i < 5; i++)
+  {
+    get_closest_food(food_points, 5, organism_locations[i], &closest_food_location);
+    printf("Location of organism: [%d %d],  Closest food target: [%d %d]\n", organism_locations[i].x, organism_locations[i].y, closest_food_location.x, closest_food_location.y);
+  }
 
   return 0;
 }
