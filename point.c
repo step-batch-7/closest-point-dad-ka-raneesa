c3ebafee7416eb0f53d12fc102eca1fbe5b8b6da
Deepika <bhuvanadeepika017@gmail.com>
2020-04-13 11:54:06 +0530

    added #define MAX_LENGTH and used

--- point.c
+++ point.c
@@ -10,7 +10,7 @@ double distance_of_points(struct Point point1, struct Point point2)
 
 void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
 {
-  double distance = 1000000.00;
+  double distance = MAX_DISTANCE;
   for (int i = 0; i < points_length; i++)
   {
     double distance_to_current_food = distance_of_points(current_location, food_points[i]);
